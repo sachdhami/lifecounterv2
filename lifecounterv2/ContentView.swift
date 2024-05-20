00859737d042d4056d9f9dbb420890e1faf2c687
Sachin Dhami <dhamis@uw.edu>
2024-05-20 13:09:50 -0700

    Initial Commit

---
+++ lifecounterv2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lifecounterv2
+//
+//  Created by Sachin Dhami on 5/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lifecounterv2/lifecounterv2App.swift
@@ -0,0 +1,17 @@
+//
+//  lifecounterv2App.swift
+//  lifecounterv2
+//
+//  Created by Sachin Dhami on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct lifecounterv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
